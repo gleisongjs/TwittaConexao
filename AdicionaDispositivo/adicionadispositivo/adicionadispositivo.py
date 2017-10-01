3361e3510da23929e132fe821c34afb42b8f6eb6
gleisongjs <gleisonans@hotmail.com>
2017-10-01 17:55:49 -0300

    subindo o AdicionaDispositivos

---
+++ AdicionaDispositivo/adicionadispositivo/adicionadispositivo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#-*-coding: utf-8 -*-
+#Criado por GleisonJSilva
+#Criado em : 01/10/2017, 17:37:20
+
+import psycopg2
+con = psycopg2.connect(host='192.168.0.14', port= 5432, 
+database='twittando',
+user='gleisongjs', password='jsilva996')
+cur = con.cursor()
+#sql = 'create table mac (id serial primary key, mac varchar(20), nome varchar(50))'
+#cur.execute(sql)
+macDispositivo = str(raw_input('digite o mac: '))
+nomeDispositivo = str(raw_input('digite o nome: '))
+sql = 'insert into mac(mac, nome) values(%s, %s)'
+sqlDados = (macDispositivo, nomeDispositivo)
+cur.execute(sql, sqlDados)
+con.commit()
+cur.execute('select * from mac')
+recset = cur.fetchall()
+for rec in recset:
+    print (rec)
+con.close()
