01a3cb2ef83ba7ce9bbe312270111e55436bea07
gleisongjs <gleisonans@hotmail.com>
2017-12-28 02:10:19 -0200

    alterando o texto do twitter

--- AddDispositivo/adddispositivo/Banco.py
+++ AddDispositivo/adddispositivo/Banco.py
@@ -8,5 +8,5 @@ import psycopg2
 class Banco():
 
     def __init__(self):
-        #self.con = psycopg2.connect(host='localhost', database='postgres',user = 'gleisongjs', password = 'jsilva996') #DB no Asus
-        self.con = psycopg2.connect(host='localhost', database='dispositivo',user = 'gleisongjs', password = 'jsilva996')#Db do Servidor
+        self.con = psycopg2.connect(host='localhost', database='postgres',user = 'gleisongjs', password = 'jsilva996') #DB no Asus
+        #self.con = psycopg2.connect(host='localhost', database='dispositivo',user = 'gleisongjs', password = 'jsilva996')#Db do Servidor
