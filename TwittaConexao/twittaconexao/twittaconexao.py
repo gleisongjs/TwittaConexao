a9169caf7ff7ea6f11e71b328844d5615958f196
gleisongjs <gleisonans@hotmail.com>
2017-12-28 03:07:50 -0200

    alterando o texto do twitter

--- TwittaConexao/twittaconexao/twittaconexao.py
+++ TwittaConexao/twittaconexao/twittaconexao.py
@@ -61,7 +61,7 @@ dispositivosDesconhecidos = ', '.join(dispositivosDesconhecidos)
 
 #Twitando 
 if aux == 1:
-    status = api.PostUpdate('''@GleisonJSilva olhe que está em casa agora: '''+dispositivosConhecidos)
+    status = api.PostUpdate('''@GleisonJSilva olhe quem está em casa agora: '''+dispositivosConhecidos)
     print status.text
 elif aux==2:
     status = api.PostUpdate('@GleisonJSilva não conhecemos o dispositivo: '+ dispositivosDesconhecidos +' - Ele está utilizando a rede agora.')
